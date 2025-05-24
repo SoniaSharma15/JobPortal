48909215c1c22c61023a051572df590cc4463430
SoniaSharma15 <148994808+SoniaSharma15@users.noreply.github.com>
2025-05-24 20:34:08 +0530

    Add files via upload

---
+++ Backend/middleware/isAuthenticated.js
@@ -0,0 +1,29 @@
+//middleware work between request and response
+
+import jwt from "jsonwebtoken";
+
+const isAuthenticated=async(req,res,next)=>{
+    try {   
+        const token=req.cookies.token;
+        if(!token){
+            return res.status(401).json({
+                msg:"Unauthorized Access ",
+                success:false
+            })
+        }
+        const decode= jwt.verify(token,process.env.SECRET_KEY);
+        if(!decode){
+            return res.status(401).json({
+                msg:"Invalid Token",
+                success:false
+            })
+        }
+        req.id=decode.userId;
+        next(); 
+        
+        //sending forward to route
+    } catch (error) {
+       console.log("validation error"+error) 
+    }
+}
+export default isAuthenticated;
