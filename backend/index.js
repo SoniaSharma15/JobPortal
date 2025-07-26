665c4aa07f76727de44bbe4fd2860b3277c828b1
SoniaSharma15 <148994808+SoniaSharma15@users.noreply.github.com>
2025-07-26 22:30:11 +0530

    update responsiveness

--- backend/index.js
+++ backend/index.js
@@ -16,8 +16,8 @@ app.use(express.json())
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions={
-    // origin:"https://job-portal-sonia-sharmas-projects.vercel.app",
-    origin:"http://localhost:5173",
+    origin:"https://job-portal-sonia-sharmas-projects.vercel.app",
+    // origin:"http://localhost:5173",
     credentials:true
 }
 app.use(cors(corsOptions));
